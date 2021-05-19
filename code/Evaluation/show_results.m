055ad362cc1d5a50935cc38a5c31921baeab094c
xingyuChenju <22925152+xingyuChenju@users.noreply.github.com>
2021-05-19 11:01:03 +0800

    2222

--- code/Evaluation/show_results.m
+++ code/Evaluation/show_results.m
@@ -10,7 +10,7 @@ fun_draw_data(k);
 %% Plot average temperature errors of 20 tags
 errors = zeros(1,20);
 for i = 1:20
-    errors(i) = mean(fun_get_pertag_error(i));
+    errors(i) = mean(fun_get_error(i));
 end
 figure
 cl = {[60,87,167]/255, [253,238,16]/255, [43,46,128]/255, [254,191,16]/255,[221,31,38]/255,[111,204,221]/255, [126,20,22]/255,[56,79,162]/255,[118,201,182]/255, [143,203,127]/255,[63,101,176]/255,[70,133,198]/255, [245,127,32]/255,[238,47,36]/255,[219,225,32]/255, [46,64,157]/255,[56,198,244]/255,[174,30,35]/255,[240,83,35]/255,[171,209,82]/255};
@@ -22,35 +22,30 @@ xticks(1:20)
 xlim([0 21])
 fun_set_axis_size('Tag #','Temperature Error (\circC)',12,[420 300]);
 
-%% Per-tag Calibration vs. One-shot Calibration
+%% Plot temperature errors at different temperature levels 
 errors = zeros(1,20);
-IDset = [1 2 3 4 5 6 7 8 9 10]; % Tag set used to obtain the parameters
 pertag_errors = [];
-oneshot_errors = [];
 temps = [];
 for i = 1:20
-    [errors,temp]= fun_get_pertag_error(i);
-    errors2 = fun_get_oneshot_error(IDset,i);
+    [errors,temp]= fun_get_error(i);
     temps = [temps;temp];
     pertag_errors = [pertag_errors;errors];
-    oneshot_errors = [oneshot_errors;errors2];
 end
-mean_errors = zeros(2,8);
+mean_errors = zeros(1,8);
 for i = 1:8
     index = temps>i*10-10&temps<i*10;
     mean_errors(1,i) = mean(pertag_errors(index));
-    mean_errors(2,i) = mean(oneshot_errors(index));
 end
 
 figure 
 cl = {[50,100,180]/255, [46,139,87]/255,  [210,105,30]/255, [128,128,128]/255,[205,92,92]/255}; % blue gree orange grey 
-h=bar(mean_errors',0.8,'FaceColor','flat');
+h=bar(mean_errors',0.6,'FaceColor','flat');
 h(1).CData = cl{1};
-h(2).CData = cl{4};
 
 xticklabels({'0-10';'10-20';'20-30';'30-40';'40-50';'50-60';'60-70';'70-80'})
-legend('Per-tag Calibration','One-shot Calibration')
-fun_set_axis_size('Temperature (\circC)','Error (\circC)',16,[420 300]);
-set(gca,'FontSize',12,'fontname','Times New Roman');
-ylim([0 12])
+set(gca,'FontSize',10,'fontname','Times New Roman');
+set(gcf,'Position', [100 100 420 300]); 
+xlabel('Temperature (\circC)','Fontsize',16,'fontname','Times New Roman');
+ylabel('Error (\circC)','Fontsize',16,'fontname','Times New Roman');
+ylim([0 6])
 
